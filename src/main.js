2d0900bd0e3c3ebc17b2f2ff8badd905dc590694
Ravi Anand <ravi.anand@nbt.ag>
2019-08-11 16:17:26 +0200

    Add vue-notification
    lint files

--- src/main.js
+++ src/main.js
@@ -2,11 +2,16 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import vuetify from './plugins/vuetify';
+import api from './api'
+import vuetify from './plugins/vuetify'
+import Notifications from 'vue-notification'
+
+Vue.use(Notifications)
 
 Vue.config.productionTip = false
 
 new Vue({
+  api,
   router,
   store,
   vuetify,
